29c37e3166e963dca4dcfd9c3acb3ae6bfe868b2
Ayush Yadav <ay2005154@gmail.com>
2026-08-30 10:08:39 +0530

    Time: 0 ms (100%), Space: 44.1 MB (36.39%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int x1=target[0];
+        int y1=target[1];
+        int ownDist=Math.abs(x1-0)+Math.abs(y1-0);
+        ArrayList<Integer>ghostDist=new ArrayList<>();
+        for(int i=0;i<ghosts.length;i++){
+            int x2=ghosts[i][0];
+            int y2=ghosts[i][1];
+            int dist=Math.abs(x1-x2)+Math.abs(y2-y1);
+            if(dist<=ownDist)return false;
+
+        }
+        return true;
+       
+        
+    }
+}
